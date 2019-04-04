4bac6e9609f8dccc49b29d0f6b071685873594cc
Vladimir Andrade <vaandrade@mun.ca>
2019-04-04 16:52:00 -0230

    Changes to SetupQuizFlask.py and templates
    I've managed to get my flask front-end working for the most part.
    
    The launch page will allow the user to enter these things:
    
    quizname
    quiz attempts
    quiz start and end date
    after clicking create quiz it will take them to an intermediary page that will allow them to type in manually the students who can take this quiz.
    After the instructor is done with that, they can click on continue to go the question creation part of the quiz creation.
    
    In quiz creation then can enter the Question, its weight, and a/an answer(s). To enter multiple answers they must first click add answer. Add answer will hold over their question and weight enter.
    
    Once they are satisifed with the amount of answers that asre stored they can enter choices separated by commas.
    
    After that they can click on "Save Question" which will store all the data they have entered and allow them enter a new Question.
    
    If they wish to finalize the quiz, they can click finalize and save their quiz which will boot them back to the inital Quiz launch page to setup a new one or logout.
    
    If you have any questions feel free to mention me. I hopep this is suitable enough documentation for you all to understand.

--- Vladimir/assignment6/createQuizFlask/SetupQuizFlask.py
+++ Vladimir/assignment6/createQuizFlask/SetupQuizFlask.py
@@ -3,8 +3,13 @@ from datetime import *
 from flask import Flask,render_template,request,flash,url_for
 
 app = Flask(__name__) 
-quizobj = None
 
+#These are abritrary values to initialize the object
+quizobj = CreateQuiz("PlaceHolderName","2","2019,3,30","2019,3,32")
+
+#Initializes ansList so it can be used when the instructor
+#Adds more than one answer
+ansList = []
 @app.route('/',methods=['GET','POST'])
 def launchPage():
     return render_template('setup_quiz.html')
@@ -15,17 +20,48 @@ def instanceQuiz():
     attempts = request.form.get('attempts')
     stime = request.form.get('stime')
     etime = request.form.get('etime')
-    quizobj = CreateQuiz(quizname,attempts,stime,etime)
-    
+    quizobj._setQuizName(quizname)
+    quizobj._setAttempts(attempts)
+    quizobj._setStartTime(stime)
+    quizobj._setEndTime(etime)
+
     return render_template('restrict_access.html')
 
 @app.route('/ra',methods=['GET','POST'])
 def giveAccess():
-     
+    quizobj._giveAccess(request.form.get('studentName'))
+    return render_template('restrict_access.html')
 
 @app.route('/addQuestion', methods=['GET','POST'])
 def creationPage():
     return render_template('QuestionCreation.html')
+
+@app.route('/addAnswer',methods=['GET','POST'])
+def addAnswer():
+    swap1 = request.form.get('question')
+    swap2 = request.form.get('weight')
+    answer = request.form.get('answer')
+    ansList.append(answer)
+    return render_template('QuestionCreation.html',
+    question=swap1,weight=swap2)
+
+@app.route('/newQuestion',methods=['GET','POST'])
+def newQuestion():
+    question = request.form.get('question')
+    weight = request.form.get('weight')
+    choices = request.form.get('choices')
+    choiceList = choices.split(",")
+
+    quizobj._addQuestion(question,weight)
+    quizobj._addChoices(choiceList)
+    quizobj._addAnswerKey(ansList)
+
+    return render_template('QuestionCreation.html')
+
+@app.route('/newquiz',methods=['GET','POST'])
+def newQuiz():
+    return render_template('setup_quiz.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Vladimir/assignment6/static/addAnswer.js
+++
@@ -1,6 +0,0 @@
-var x=1
-function addAnswer()
-{
-    var d = document.getElementById('answerDiv');
-    d.innerHTML += "Answer+x+<input type='text' id='addAnswer"+ x++ +"'><br >";
-}
