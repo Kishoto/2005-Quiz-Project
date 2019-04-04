f61b8afcc3569058b84bcafa3bee3ee8cb0c4ed5
WeicaiZhao <wz1842@mun.ca>
2019-04-04 15:58:12 -0230

    add documents for Question 5 and modified some docstrings

--- Danny/2005_Project/login.py
+++ Danny/2005_Project/login.py
@@ -14,6 +14,7 @@ class Login:
         password - user set it to log into the system
 
     Public Methods:
+        authentication() - authenticate the password is valid or not
         createAccount() - create the new account
         userlogin() - user login method
     """
@@ -24,7 +25,7 @@ class Login:
         initialize the class and indicates all the parameters
         parameter:
             userType: type of user account: instructor or student
-            email: user identity, also the key of the account dictionary
+            userID: user identity, also the key of the account dictionary
             password: value of the key(email)
 
         """
@@ -44,7 +45,7 @@ class Login:
         create account method, which includes two usertype: student and instructor
         parameter:
             userType: type of user account: instructor or student
-            email: user identity, also the key of the account dictionary
+            userID: user identity, also the key of the account dictionary
             password: value of the key(email)
         """
         
@@ -57,17 +58,16 @@ class Login:
         elif userType == "student":
             if len(userID) > 0:
                 Login.loginStorage.addStudentAccount(userID, password)
-            #print("Account has been created")
             return True
 
 
 
     def userlogin (self, userKey, password):
         """
-        This method is login method includes password authentication, but it is not finished yet
-        parameter:
-            userType: type of user account: instructor or student
-            userID: user identity, also the key of the account dictionary
+        This method is login method to check whether the userID is in persist or not and check whether the password
+        is equal to what the user set
+            userKey: The username which get from the persist
+            password: The password which also get from the persist
         """
         studentAccount = Login.loginStorage.getStudentAccount()
         instructorAccount = Login.loginStorage.getInstructorAccount()
