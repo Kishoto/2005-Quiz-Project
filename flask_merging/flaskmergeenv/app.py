0acaeb46d84caf8c69cc86cb82486029a09c0f44
Kishoto <45502489+Kishoto@users.noreply.github.com>
2019-04-05 23:32:02 -0230

    Final Updates

--- flask_merging/flaskmergeenv/app.py
+++ flask_merging/flaskmergeenv/app.py
@@ -4,6 +4,7 @@ from login import *
 from datetime import datetime
 from CreateQuiz import *
 from Take_Quiz import *
+from QuizResult import *
 
 import os
 ansList = []
@@ -13,6 +14,12 @@ instructorAccount = Login.loginStorage.getInstructorAccount()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+obj = QuizResult()
+obj.add_quiz_attempts()
+obj.get_grades()
+
+quizname = ''
 
 @app.route('/', methods=['GET', 'POST'])
 def base():
@@ -251,6 +258,73 @@ def display_done():
             activeQuiz.stopQuiz()
         jAttempts = TakeQuiz.studentsQA[studentName][quizName]
         return render_template('done.html', jAttempts=jAttempts, finished=finished)
+
+@app.route("/ins_class_part",methods=['GET', 'POST'])
+def class_part():
+    quizname = request.form.get('quizname')
+    c = obj.ins_class_part(quizname)
+    d = {'grade': c}
+    
+    return render_template('ins_class_part.html', title='Class Participation',call = d,name = quizname)
+
+
+@app.route("/ins_class_hist",methods=['GET', 'POST'])
+def class_hist():
+    quizname = request.form.get('quizname')
+    c = obj.ins_class_hist(quizname)
+    d = [{'first': c}]
+    
+    return render_template('ins_class_hist.html', title='Class Histogram',call = d,name = quizname)
+
+@app.route("/ins_class_attempts",methods=['GET', 'POST'])
+def class_attempts():
+    quizname = request.form.get('quizname')
+    c = obj.ins_class_attempts(quizname)
+    if len(c) < 1:
+        d = ['a','b']
+    else:
+        d = [{'first': c}]
+    
+    return render_template('ins_class_attempts.html', title='Class Attempts',call = d,name = quizname)
+
+@app.route("/ins_student_grades",methods=['GET', 'POST'])
+def student_grades():
+    stuname = request.form.get('stuname')
+    c = obj.ins_student_grades(stuname)
+
+    if len(c) < 1:
+        d = ['a','b']
+    else:
+        d = [{'first': c}]
+
+    return render_template('ins_student_grades.html', title='Student Grades',call = d,name = stuname)
+
+
+@app.route("/stu_quiz_grades",methods=['GET', 'POST'])
+def grade_summaries():
+    stuname = request.form.get('stuname')
+    c = obj.stu_quiz_grades(stuname)
+
+    if len(c) < 1:
+        d = ['a','b']
+    else:
+        d = [{'first': c}]
+
+    return render_template('stu_quiz_grades.html', title='Student Grades',call = d,name = stuname)
+
+@app.route("/stu_quiz_detailed",methods=['GET', 'POST'])
+def detail_summaries():
+    stuname = request.form.get('stuname')
+    quizname = request.form.get('quizname')
+    
+    c = obj.stu_quiz_detailed(stuname,quizname)
+
+    if len(c) < 1:
+        d = ['a','b']
+    else:
+        d = [{'first': c}]
+
+    return render_template('stu_quiz_detailed.html', title='Student Grades',call = d,name = stuname)
 
 
 
